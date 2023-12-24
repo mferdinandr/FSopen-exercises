93e6fea8c8037104d1117a3058d46b8d5e7d696b
Ferdinand Ramdhani <ferdinram13@gmail.com>
2023-12-24 23:44:41 +0700

    add - 2.4

--- 2.1-2.5/src/components/Course.jsx
+++ 2.1-2.5/src/components/Course.jsx
@@ -1,40 +1,81 @@
+// const Header = ({ text }) => {
+//   return <h2>{text}</h2>;
+// };
+
+// const Content = ({ items }) => {
+//   return (
+//     <>
+//       {items.map((item, i) => (
+//         <Part key={i} course={item} />
+//       ))}
+//     </>
+//   );
+// };
+
+// const Part = ({ course }) => {
+//   return (
+//     <p>
+//       {course.name} {course.exercises}
+//     </p>
+//   );
+// };
+
+// const Total = ({ times }) => {
+//   return (
+//     <b>
+//       Total exercises : {times.reduce((acc, sum) => acc + sum.exercises, 0)}{' '}
+//       exercises
+//     </b>
+//   );
+// };
+
+// const Course = ({ course }) => {
+//   return (
+//     <>
+//       <Header text={course.name} />
+//       <Content items={course.parts} />
+//       <Total times={course.parts} />
+//     </>
+//   );
+// };
+
+// 2
 const Header = ({ text }) => {
   return <h2>{text}</h2>;
 };
 
-const Content = ({ items }) => {
+const Content = ({ courses }) => {
+  // console.log('aaa', courses);
   return (
     <>
-      {items.map((item, i) => (
-        <Part key={i} course={item} />
+      {courses.map((course, i) => (
+        <Part key={i} item={course.parts} />
       ))}
     </>
   );
 };
 
-const Part = ({ course }) => {
-  return (
-    <p>
-      {course.name} {course.exercises}
-    </p>
-  );
-};
-
-const Total = ({ times }) => {
-  return (
-    <b>
-      Total exercises : {times.reduce((acc, sum) => acc + sum.exercises, 0)}{' '}
-      exercises
-    </b>
-  );
-};
-
-const Course = ({ course }) => {
+const Part = ({ item }) => {
   return (
     <>
-      <Header text={course.name} />
-      <Content items={course.parts} />
-      <Total times={course.parts} />
+      {item.map((course, i) => (
+        <p key={i}>
+          {course.name} {course.exercises}
+        </p>
+      ))}
+    </>
+  );
+};
+
+const Course = ({ courses }) => {
+  return (
+    <>
+      {courses.map((course, i) => (
+        <div key={i}>
+          <Header text={course.name} />
+        </div>
+      ))}
+      <Content courses={courses} />
     </>
   );
 };
