f5538f756f631758f6ce7685cc80bc1e982391c7
Ferdinand Ramdhani <ferdinram13@gmail.com>
2023-12-29 10:02:08 +0700

    finish 2.11

--- 2.6-2.10/src/App.jsx
+++ 2.6-2.10/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Filter } from './components/Filter';
 import { PersonForm } from './components/PersonForm';
 import { Persons } from './components/Persons';
+import axios from 'axios';
 
 const App = () => {
   const [id, setId] = useState(0);
   const [persons, setPersons] = useState([]);
-
   const [newName, setNewName] = useState('');
   const [number, setNumber] = useState('');
   const [personFilter, setPersonFilter] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleChangeName = (event) => {
     setNewName(event.target.value);
