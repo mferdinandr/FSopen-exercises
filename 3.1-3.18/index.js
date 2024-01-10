c7414150d8bb203b8465ff03a02202aa83e353c3
Ferdinand Ramdhani <ferdinram13@gmail.com>
2024-01-10 10:07:48 +0700

    finish 3.18

--- 3.1-3.18/index.js
+++ 3.1-3.18/index.js
@@ -90,7 +90,17 @@ app.post('/api/persons', (request, response, next) => {
     });
 });
 
-
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
