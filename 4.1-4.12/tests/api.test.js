9af7614a9aa832ef090e18a10d961a685afe0c5a
Ferdinand Ramdhani <ferdinram13@gmail.com>
2024-01-16 15:36:42 +0700

    finish 4.12

--- 4.1-4.12/tests/api.test.js
+++ 4.1-4.12/tests/api.test.js
@@ -99,87 +99,94 @@ describe('posters', () => {
     expect(response.body.likes).toBe(0);
   });
 
-  // test('blog without title is not added', async () => {
-  //   const newBlog = {
-  //     author: 'Valid author',
-  //     url: 'www.validUrl.com',
-  //     likes: 1,
-  //   };
+  test('blog without title is not added', async () => {
+    const newBlog = {
+      author: 'Valid author',
+      url: 'www.validUrl.com',
+      likes: 1,
+    };
 
-  //   await api.post('/api/blogs').send(newBlog).expect(400);
+    await api.post('/api/blogs').send(newBlog).expect(400);
 
-  //   const blogsAtEnd = await helper.blogsInDb();
-  //   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
-  // });
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
 
-  // test('blog without title and url returns status 400', async () => {
-  //   const newBlog = {
-  //     author: 'Valid author',
-  //     likes: 1,
-  //   };
+  test('blog without title and url returns status 400', async () => {
+    const newBlog = {
+      author: 'Valid author',
+      likes: 1,
+    };
 
-  //   await api.post('/api/blogs').send(newBlog).expect(400);
-  // });
+    await api.post('/api/blogs').send(newBlog).expect(400);
+  });
 
-  // test('saved blog has correct format', async () => {
-  //   const newBlog = {
-  //     title: 'Correctly saved blog',
-  //     author: 'Mr. Correct',
-  //     url: 'www.correctly-saved.com',
-  //     likes: 99,
-  //   };
+  test('saved blog has correct format', async () => {
+    const newBlog = {
+      title: 'Correctly saved blog',
+      author: 'Mr. Correct',
+      url: 'www.correctly-saved.com',
+      likes: 99,
+    };
 
-  //   const savedBlog = await api
-  //     .post('/api/blogs')
-  //     .send(newBlog)
-  //     .expect(200)
-  //     .expect('Content-Type', /application\/json/);
+    const savedBlog = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
-  //   expect(savedBlog.body.title).toBe('Correctly saved blog');
-  //   expect(savedBlog.body.author).toBe('Mr. Correct');
-  //   expect(savedBlog.body.url).toBe('www.correctly-saved.com');
-  //   expect(savedBlog.body.likes).toBe(99);
-  //   expect(savedBlog.body.id).toBeDefined();
-  // });
+    expect(savedBlog.body.title).toBe('Correctly saved blog');
+    expect(savedBlog.body.author).toBe('Mr. Correct');
+    expect(savedBlog.body.url).toBe('www.correctly-saved.com');
+    expect(savedBlog.body.likes).toBe(99);
+    expect(savedBlog.body.id).toBeDefined();
+  });
 });
 
-// describe('deleters', () => {
-//   test('a blog can be deleted', async () => {
-//     const blogsAtStart = await helper.blogsInDb()
-//     const blogToDelete = blogsAtStart[0]
+describe('deleters', () => {
+  test('a blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
 
-//     await api
-//       .delete(`/api/blogs/${blogToDelete.id}`)
-//       .expect(204)
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
 
-//     const blogsAtEnd = await helper.blogsInDb()
+    const blogsAtEnd = await helper.blogsInDb();
 
-//     expect(blogsAtEnd).toHaveLength(
-//       helper.initialBlogs.length - 1
-//     )
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
 
-//     const titles = blogsAtEnd.map(blog => blog.title)
+    const titles = blogsAtEnd.map((blog) => blog.title);
 
-//     expect(titles).not.toContain(blogToDelete.title)
-//   })
-// })
+    expect(titles).not.toContain(blogToDelete.title);
+  });
+});
 
-// describe('putters', () => {
-//   test('a blog can be updated', async () => {
-//     const blogsAtStart = await helper.blogsInDb()
-//     const blogToUpdate = blogsAtStart[0]
-//     const update = { title: 'Changed Title', author: 'Changed Author', url: 'www.changed.com', likes: blogToUpdate.likes + 500, }
+describe('putters', () => {
+  test('a blog can be updated', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToUpdate = blogsAtStart[0];
+    const update = {
+      title: 'Changed Title',
+      author: 'Changed Author',
+      url: 'www.changed.com',
+      likes: blogToUpdate.likes + 500,
+    };
 
-//     const updatedBlog = await api
-//       .put(`/api/blogs/${blogToUpdate.id}`)
-//       .send(update)
-//       .expect(200)
-//       .expect('Content-Type', /application\/json/)
+    const updatedBlog = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(update)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
 
-//     expect(updatedBlog.body).toEqual({ title: 'Changed Title', author: 'Changed Author', url: 'www.changed.com', likes: blogToUpdate.likes + 500, id: blogToUpdate.id })
-//     expect(updatedBlog.body).not.toBe(blogToUpdate)
-//   })
-// })
+    expect(updatedBlog.body).toEqual({
+      title: 'Changed Title',
+      author: 'Changed Author',
+      url: 'www.changed.com',
+      likes: blogToUpdate.likes + 500,
+      id: blogToUpdate.id,
+    });
+    expect(updatedBlog.body).not.toBe(blogToUpdate);
+  });
+});
 
 describe('structure tests', () => {
   test('blogs should have id but not _id', async () => {
