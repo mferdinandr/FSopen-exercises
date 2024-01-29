ae82668baa8f18773c75577d46513c725f5739bf
Ferdinand Ramdhani <ferdinram13@gmail.com>
2024-01-29 09:50:40 +0700

    finish - 6.8

---
+++ 6.3-6.8/src/components/AnecdoteList.jsx
@@ -0,0 +1,28 @@
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { useDispatch } from 'react-redux';
+import { voteNote } from '../reducers/anecdoteReducer';
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const vote = (id) => {
+    dispatch(voteNote(id));
+  };
+
+  return (
+    <>
+      {anecdotes.sort(anecdotes.votes).map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default AnecdoteList;
