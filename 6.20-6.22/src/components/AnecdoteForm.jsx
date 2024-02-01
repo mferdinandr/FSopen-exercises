5536c7313105a846cc1832952ebea64167368bcc
Ferdinand Ramdhani <ferdinram13@gmail.com>
2024-02-01 10:28:58 +0700

    finish already - refactoring async

--- 6.20-6.22/src/components/AnecdoteForm.jsx
+++ 6.20-6.22/src/components/AnecdoteForm.jsx
@@ -16,7 +16,7 @@ const AnecdoteForm = () => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = '';
-    newAnecdoteMutation.mutate({ content, votes: 0 });
+    newAnecdoteMutation.mutate(content);
   };
 
   return (
--- 6.20-6.22/src/services/anecdote-service.js
+++ 6.20-6.22/src/services/anecdote-service.js
@@ -1,11 +1,27 @@
 import axios from 'axios';
 
 const baseUrl = 'http://localhost:3001/anecdotes';
+const getId = () => (100000 * Math.random()).toFixed(0);
 
-export const getAnecdotes = () => axios.get(baseUrl).then((res) => res.data);
+export const getAnecdotes = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
 
-export const createAnecdotes = (newAnecdote) =>
-  axios.post(baseUrl, newAnecdote).then((res) => res.data);
+export const createAnecdotes = async (content) => {
+  const newAnecdote = {
+    content,
+    id: getId(),
+    votes: 0,
+  };
+  const response = await axios.post(baseUrl, newAnecdote);
+  return response.data;
+};
 
-export const updateAnecdote = (updatedAnecdote) =>
-  axios.put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote).then(res => res.data);
+export const updateAnecdote = async (updatedAnecdote) => {
+  const response = axios.put(
+    `${baseUrl}/${updatedAnecdote.id}`,
+    updatedAnecdote
+  );
+  return response.data;
+};
