405d1c493ab6874fcf37988dfe5d007c72435312
Ferdinand Ramdhani <ferdinram13@gmail.com>
2024-02-04 14:11:19 +0700

    finish 7.2

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -15,16 +15,15 @@ const Menu = () => {
   };
   return (
     <div>
-      <Link to={'/anecdotes'} style={padding}>
+      <Link to={'/'} style={padding}>
         anecdotes
       </Link>
       <Link to={'/create'} style={padding}>
         create new
       </Link>
-
-      <a href="#" style={padding}>
+      <Link to={'/about'} style={padding}>
         about
-      </a>
+      </Link>
     </div>
   );
 };
@@ -50,13 +49,35 @@ const AnecdoteList = ({ anecdotes }) => {
   );
 };
 
-const About = ({ anecdote }) => (
+const Anecdote = ({ anecdote }) => (
   <div>
     <h2>{anecdote.content}</h2>
     <p>has {anecdote.votes} votes</p>
 
     <p>
       for more info see <a href={anecdote.info}>{anecdote.info}</a>
+    </p>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About anecdote app</h2>
+    <p>According to Wikipedia:</p>
+
+    <em>
+      An anecdote is a brief, revealing account of an individual person or an
+      incident. Occasionally humorous, anecdotes differ from jokes because their
+      primary purpose is not simply to provoke laughter but to reveal a truth
+      more general than the brief tale itself, such as to characterize a person
+      by delineating a specific quirk or trait, to communicate an abstract idea
+      about a person, place, or thing through the concrete details of a short
+      narrative. An anecdote is "a story with a point."
+    </em>
+
+    <p>
+      Software engineering is full of excellent anecdotes, at this app you can
+      find the best and add more.
     </p>
   </div>
 );
@@ -170,12 +191,12 @@ const App = () => {
       <Menu />
       <Routes>
         <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
-        <Route
-          path="/anecdotes"
-          element={<AnecdoteList anecdotes={anecdotes} />}
-        />
         <Route path="/create" element={<CreateNew addNew={addNew} />} />
-        <Route path="/anecdotes/:id" element={<About anecdote={anecdote} />} />
+        <Route
+          path="/anecdotes/:id"
+          element={<Anecdote anecdote={anecdote} />}
+        />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </div>
